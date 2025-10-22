ac6aeca9ed226ea7ff6d2d9636f314bc0aa00ffa
Shaikh Abdul Hanaan <161044491+beinghanaan5500@users.noreply.github.com>
2025-10-22 14:37:11 +0530

    Add files via upload

---
+++ exp05wc/hanaan-registration-form/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RegistrationForm from "./RegistrationForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hanaan Registration Form</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
